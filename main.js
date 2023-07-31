620bb950891b1eaa54cb0c1b7bf7cd241fac2efe
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2023-07-30 22:24:46 -0500

    feat: added sql storage and colorized log output

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 require("dotenv").config()
+const WebSocket = require("ws")
+const mysql = require("mysql2")
+const mySQLConfig = require("./config.json")
 const Accounts = require("./accounts.js")
 const domain =  process.env.DOMAIN_NAME || "gmail.com"
-const ratelimit = +process.env.RATELIMIT || 1000 //Speed in milliseconds
+const ratelimit = +process.env.RATELIMIT || 1000
+const verificationURL = process.env.VERIFICATION_SERVER_URL || ""
+const debugMode = process.env.DEBUG_MODE || false
+var connection;
+var accsCreating = []
 
 async function main() {
     try {
@@ -13,25 +20,34 @@ async function main() {
         let [json, username, password] = account
         switch (json.success) {
             case true:
-                console.log(`Account created successfully! Username: ${username} Password: ${password}`)
+                coloredLog(`Account created successfully! Username: ${username} Password: ${password}`, "cyan")
                 return {username, password}
             case false:
-                console.log(`Account creation failed. Reason: ${json.error.reason}`)
+                coloredLog(`Account creation failed. Reason: ${json.error.reason}`, "red")
                 return null
             default:
-                console.log("Account creation failed. Reason: Unknown")
+                coloredLog("Account creation failed. Reason: Unknown", "red")
                 return null
         }
     }
     catch (err) {
-        console.log("Could not create account. Check if Tor is running.")
+        coloredLog("Could not create account. Check if Tor is running.", "red")
         return null
     }
 }
 
 async function genLoop() {
     let startTime = Date.now()
-    await main()
+    let creds = await main()
+    if (creds && !debugMode) {
+        accsCreating.push(creds.username)
+        let query = `INSERT INTO accounts (username, password, isVerified) VALUES ('${creds.username}', '${creds.password}', 0)`
+        connection.query(query, (err, result) => {
+            if (err) return
+            accsCreating.splice(accsCreating.indexOf(creds.username), 1)
+            coloredLog(`Added ${creds.username} to database.`, "green")
+        })
+    }
     let timeElapsed = Date.now() - startTime
     if (timeElapsed < ratelimit)
         setTimeout(genLoop, ratelimit - timeElapsed)
@@ -39,4 +55,66 @@ async function genLoop() {
         genLoop()
 }
 
-genLoop()
+genLoop();
+
+(function handleDisconnect() {
+    connection = mysql.createConnection(mySQLConfig)
+    connection.connect(function (err) {
+        if (err) setTimeout(handleDisconnect, 2000)
+    })
+    connection.on('error', function (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') handleDisconnect()
+        else throw err
+    })
+})();
+
+(function handleSocketDisconnect() {
+    let socket = new WebSocket(verificationURL)
+    socket.onmessage = (e) => {
+        if (!debugMode) {
+            if (accsCreating.includes(e.data)) {
+                let iter = 0
+                let tryVerifyLoop = setInterval(() => {
+                    if (!accsCreating.includes(e.data)) {
+                        clearInterval(tryVerifyLoop)
+                        verifyQuery(e.data)
+                    }
+                    if (iter >= 30) clearInterval(tryVerifyLoop)
+                    iter++
+                }, 1000)
+            }
+            else verifyQuery(e.data)
+        }
+    }
+    socket.onclose = () => {
+        setTimeout(handleSocketDisconnect, 2000)
+    }
+    socket.onerror = () => {}
+})()
+
+function verifyQuery(username) {
+    let query = `UPDATE accounts SET isVerified = 1 WHERE username = '${username}'`
+    connection.query(query, (err, result) => {
+        if (err) return
+        coloredLog(`Verified ${username}.`, "purple")
+    })
+}
+
+function coloredLog(msg, color) {
+    let colorCode;
+    switch (color) {
+        case "cyan":
+            colorCode = "\u001b[1;36m"
+            break
+        case "green":
+            colorCode = "\u001b[1;32m"
+            break
+        case "purple":
+            colorCode = "\u001b[1;35m"
+            break
+        case "red":
+            colorCode = "\u001b[1;31m"
+            break
+    }
+    console.log(colorCode + msg + "\u001b[0m")
+}
